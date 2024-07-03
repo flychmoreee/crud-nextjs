f37e3d3d11ed65c6dd3092e3a447be69548072cb
rasya109 <satriavega2004@gmail.com>
2024-07-04 01:07:30 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol: "https",
+                hostname: "rwnj6p31wc2p4rcv.public.blob.vercel-storage.com"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
